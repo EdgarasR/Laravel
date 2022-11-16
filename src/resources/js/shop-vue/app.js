3f8cf48876e4a57c34b93914c82333ecdfe8eaa7
Edgaras <edgaras.rcns@gmail.com>
2022-11-16 20:24:53 +0200

    Pinia

--- src/resources/js/shop-vue/app.js
+++ src/resources/js/shop-vue/app.js
@@ -1,7 +1,10 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import Shop from './Shop.vue'
+import "./bootstrap";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import Shop from "./Shop.vue";
 
+const pinia = createPinia();
 const app = createApp(Shop);
 
-app.mount('#shop');
+app.use(pinia);
+app.mount("#shop");
